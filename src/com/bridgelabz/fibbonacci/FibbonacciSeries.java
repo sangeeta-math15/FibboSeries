9c757c046bb47502460decb8c65ae70f971a5aa9
sngtmth <sngtmth@gmail.com>
2021-09-30 18:14:48 +0530

    Added Weccome message

--- src/com/bridgelabz/fibbonacci/FibbonacciSeries.java
+++ src/com/bridgelabz/fibbonacci/FibbonacciSeries.java
@@ -3,25 +3,21 @@ package com.bridgelabz.fibbonacci;
 public class FibbonacciSeries {
 
 	public static void main(String[] args) {
-		
-		int n1=0,n2=1,n=5;
+		/* 0yh and 1st number of the series are 0 and 1*/
+		System.out.println("Welcome to Fibbonacci series");
+		int n1 = 0, n2 = 1, n3;
+		int count = 50;
 		System.out.println(n1);
 		System.out.println(n2);
-		findSeries(n,n1,n2);
-		
-	}
 
-	private static void findSeries(int n,int n1,int n2) {
-		int n3;
-		for(int i=2;i<n;i++)
-		{
-			n3=n1+n2;
+		for (int i = 2; i <= count; i++) {
+			/**/
+			n3 = n1 + n2;
 			System.out.println(n3);
-			n2=n3;
-			n1=n2;
-		}
-		
-		
-	}
+			n1 = n2;
+			n2 = n3;
 
+		}
+
+	}
 }
